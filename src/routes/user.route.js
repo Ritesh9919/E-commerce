665f997c2f49d0d11d2ef3fb74cb8a9026559121
Ritesh Maurya <riteshkumar411552@gmail.com>
2024-02-23 08:34:30 +0530

    created authentication middleware

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,8 +1,10 @@
 import express from 'express';
 const router = express.Router();
 import {register,login} from '../controllers/auth.controller.js';
+import {varifyJwt} from '../middlewares/auth.middleware.js'
 
 router.post('/register', register);
 router.post('/login', login);
+
 
 export default router;
