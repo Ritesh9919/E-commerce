edf5b5fbf7b6ee6426ea5654e3006cbbb1c345a4
Ritesh Maurya <riteshkumar411552@gmail.com>
2024-02-25 10:22:58 +0530

    created order schema

---
+++ src/models/order.model.js
@@ -0,0 +1,37 @@
+import mongoose from 'mongoose';
+
+const orderItemSchema = new mongoose.Schema({
+    productId:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:'Product',
+        required:true
+    },
+    quantity:{
+        type:Number,
+        required:true
+    }
+})
+
+
+const orderSchema = new mongoose.Schema({
+ orderPrice:{
+    type:Number,
+    required:true
+ },
+ userId:{
+    type:mongoose.Schema.Types.ObjectId,
+    ref:'User'
+ },
+ orderItem:[orderItemSchema],
+ address:{
+    type:String,
+    required:true
+ },
+ status:{
+    type:String,
+    enum:['PENDING','CANCELLED','DELIVERED']
+ }
+
+},{timestamps:true});
+
+export const Order = mongoose.model('Order', orderSchema);
